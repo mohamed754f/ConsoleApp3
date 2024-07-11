436bc5ec17a43f6f7eeab123f596c4c27c2108e4
Mohamed Galal <Mohamed Galal@DESKTOP-28D5B9Q>
2024-07-11 13:13:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Student(int iD, string name, int age)
+            {
+                ID = iD;
+                Name = name;
+                Age = age;
+            }
+
+            public void Prints()
+            {
+                Console.WriteLine($"ID = {ID} \nName = {Name}\nAge = {Age}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Correct way to instantiate the Student object using the constructor
+            Student student = new Student(1, "Mohamed", 20);
+
+            student.Prints();
+        }
+    }
+}
